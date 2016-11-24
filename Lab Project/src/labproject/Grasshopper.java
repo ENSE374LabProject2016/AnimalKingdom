7aec2475388cdb3c9da37c463524516a082f8826
Alhwirif <oozz.2014@gmail.com>
2016-11-23 19:00:37 -0600

    changed the letter for grasshopper form 'g' to 'p'

--- Lab Project/src/labproject/Grasshopper.java
+++ Lab Project/src/labproject/Grasshopper.java
@@ -4,11 +4,11 @@ abstract public class Insects extends Herbivores {
 
     public Grasshopper()
     {
-        name='G';
+        name='P';
     }
     
     public void move()
     {
-        this.setPos(this.getPostX()+1,this.getPostY()+1,'G');
+        this.setPos(this.getPostX()+1,this.getPostY()+1,'P');
     }
 }
